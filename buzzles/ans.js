affcc4ca8a0188143683234c271402dd01301b3e
thedylone <thedylone@gmail.com>
2021-06-16 21:49:02 +0800

    new buzzle and formatting changes

--- buzzles/ans.js
+++ buzzles/ans.js
@@ -1,14 +1,17 @@
 var ansArr = [
     {
-        "file":"badApple",
-        "title":"bad apple",
-        "solution":"dadd45c8788f828dbcbfbd3526f0bfcd28609e821047b7b97a02583bda4028f5"
+        "file":"lostWord",
+        "title":"lost word",
+        "solution":"66a287523e6af302e6dc48b2414bf43d825af64f165d974cac2cd86b09b49aa1",
+        "salt":"sh4zam"
     },
     {
-        "file":"buzzle2",
-        "title":"buzzle 2",
-        "solution":"amogus"
+        "file":"badApple",
+        "title":"bad apple",
+        "solution":"dadd45c8788f828dbcbfbd3526f0bfcd28609e821047b7b97a02583bda4028f5",
+        "salt":"thedyl1"
     },
+
     {
         "file":"buzzle3",
         "title":"buzzle 3",
