88f86529cca58fdb01518e3ac166988065810306
thedylone <thedylone@gmail.com>
2021-06-17 22:58:02 +0800

    new buzzle and more formatting

--- buzzles/ans.js
+++ buzzles/ans.js
@@ -7,14 +7,17 @@ var ansArr = [
     },
     {
         "file":"badApple",
-        "title":"bad apple",
+        "title":"🅱&#xFE0F;ad apple",
         "solution":"dadd45c8788f828dbcbfbd3526f0bfcd28609e821047b7b97a02583bda4028f5",
         "salt":"thedyl1"
     },
 
     {
-        "file":"buzzle3",
-        "title":"buzzle 3",
-        "solution":"amogus"
+        "file":"shazam",
+        "title":"shazam!",
+        "solution":"9a9f4ded7e86ece3805863919143ad856972b074c44a5f6d3c7b1a74061c9152",
+        "salt":"al1yxc14",
+        "hint":"87dd73d4d9d2b4d731084a471aba06d33a061dc775d5b99f3e85e16cad158644",
+        "prompt":"try googling KIMOTA: who's the character?"
     }
 ]
